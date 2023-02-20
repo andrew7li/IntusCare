05d372d7a4406d802041d6ef3ddb058dbb055c3d
Andrew Li <Andrewli2024@gmail.com>
2023-02-20 00:48:05 -0500

    Change backend port to 8000

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const http = require("http");
 const { app } = require("./app");
 
-const PORT = 5000;
+// TODO: Change this port back to 5000 upon submission.
+// Mac OS uses 5000 for their Airplay on their latest update.
+const PORT = 8000;
 
 function startServer() {
   const server = http.createServer(app);
