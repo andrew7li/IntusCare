6dcc2be5b518c8ef8f83cac316485f145746d393
Andrew Li <Andrewli2024@gmail.com>
2023-02-20 21:53:52 -0500

    Move images into public, make Bootstrap Navbar

---
+++ intus-client/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function Header() {
+  return (
+    <Navbar variant="light" expand="lg" className="header">
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src="/logo_IntusCare.svg"
+          width="300"
+          className="d-inline-block align-top ms-3"
+        />
+      </Navbar.Brand>
+    </Navbar>
+  );
+}
