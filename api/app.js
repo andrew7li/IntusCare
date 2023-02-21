feb6fd41a154cf295a85a9456813345cd56fa410
Andrew Li <Andrewli2024@gmail.com>
2023-02-21 00:38:09 -0500

    Build participant menu from calling API, implemented sorting

--- api/app.js
+++ api/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const { participants } = require("./data");
 
 const app = express();
+
+// Enable CORS so that the React app can make requests to the API
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  })
+);
 
 app.get("/participants", (_, res) => {
   res.json(participants);
