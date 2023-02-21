170c07f7eb02e3206be4de8cd1e812496c49d748
Andrew Li <Andrewli2024@gmail.com>
2023-02-21 11:18:30 -0500

    Remove TODO comment

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 const http = require("http");
 const { app } = require("./app");
 
-// TODO: Change this port back to 5000 upon submission.
-// Mac OS uses 5000 for their Airplay on their latest update.
+// Mac OS uses 5000 for their Airplay on their latest update, so I changed it to 8000
 const PORT = 8000;
 
 function startServer() {
